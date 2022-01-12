70a42d9dfa174361ad4d3bf6a3863b7059a945b2
95wjddbs <95wjddbs@naver.com>
2022-01-12 14:48:04 +0900

    0112

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,26 +1,15 @@
-import dummy from "../DB/data.json";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
-import { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
 
 const Day = () => {
   const { day } = useParams();
-  // const wordList = dummy.words.filter((word) => word.day === Number(day));
-  const [words, setWords] = useState([]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      })
-      .catch((e) => console.log(e));
-  }, [day]);
   return (
     <>
       <h2>Day {day}</h2>
+      {words.length === 0 && <span>Loading...</span>}
       <table>
         <tbody>
           {words.map((word) => (
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,37 +1,15 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import dummy from "../DB/data.json";
+import useFetch from "../hooks/useFetch";
 
 const DayList = () => {
-  const [days, setDays] = useState([]);
-  const [count, setCount] = useState(0);
+  const days = useFetch("http://localhost:3001/days");
 
-  const onclick = () => {
-    setCount(count + 1);
-  };
+  if (days.length === 0) {
+    return <span>Loading...</span>;
+  }
 
-  const onclick2 = () => {
-    setDays([
-      ...days,
-      {
-        id: Math.random(),
-        day: 1,
-      },
-    ]);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:3001/days")
-      .then((res) => {
-        console.log("리스폰", res);
-        return res.json();
-      })
-      .then((data) => {
-        console.log("데이타", data);
-        setDays(data);
-      })
-      .catch((e) => console.log(e));
-  }, []);
   return (
     <>
       <ul className="list_day">
